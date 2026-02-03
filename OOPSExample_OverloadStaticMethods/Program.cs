068af83482de4dbdb5814e45c0244f7de0904d11
Dell <Dell@DESKTOP-13B42NJ>
2026-02-03 07:56:16 +0530

    Add project files.

---
+++ OOPSExample_OverloadStaticMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSExample_OverloadStaticMethods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            myclass.print();//0 parameter combination method calling
+            myclass.print(10);//single parameter combination method calling
+            myclass.print(10, 20);//Two parameter combination method calling
+            myclass.print(10, 20,30);//Three parameter combination method calling
+            myclass.print(10, 20,30,40);//Four parameter combination method calling
+            myclass.print(10, 20, 30, 40,50);//Five parameter combination method calling
+        }
+    }
+}
+/*output:
+zero parameter is calling
+one parameter print :10
+Two parameter print :30
+Three parameter print :60
+Four parameter print :100
+Five parameter print :1020304050
+
+*/
---
+++ OOPSExample_OverloadStaticMethods/myclass.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPSExample_OverloadStaticMethods
+{
+    internal class myclass
+    {
+        /*
+Method Overloading It doesn't depend on method return type and static method or non-static method it will always follow method name should be same and Parameters should be different. 
+Same method combination should not repeat.
+If you repeat same combination it will throw compile time error.
+        */
+
+        internal static void print()//0 parameter static method
+        {
+            Console.WriteLine("zero parameter is calling");
+        }
+        internal static void print(int a)//single parameter static method
+        {
+            Console.WriteLine("one parameter print :" + a);
+        }
+        internal static void print(int a,int b)//Two parameter static method
+        {
+            int result = a + b;
+            Console.WriteLine("Two parameter print :" + result);
+        }
+        internal static void print(int a, int b,int c)//Three parameter static method
+        {
+            int result = a + b+c;
+            Console.WriteLine("Three parameter print :" + result);
+        }
+        internal static void print(int a, int b, int c,int d)//Four parameter static method
+        {
+            int result = a + b + c+d;
+            Console.WriteLine("Four parameter print :" + result);
+        }
+        internal static void print(int a, int b, int c, int d,int e)//Five parameter static method
+        {
+            Console.WriteLine("Five parameter print :" + a + b + c+d+e);
+        }
+        /*   don't repeat same combination in normal methods (or)static methods in overloading concept
+        internal static void print(int a)//if same combination is repeat it will throw compiletime error
+        {
+            Console.WriteLine("one parameter print :" + a);
+        }
+        */
+        //overloading doesn't depend method return type  and static or non static method.it will always depeds on methodname same and parameter should be diffrent
+       /*
+        internal string print(int a)//single parameter  non static method(here it is throwing compile time error)same combination repeated.
+        {
+            Console.WriteLine("one parameter print :" + a);
+            return "abc";
+        }
+        internal void print(int a)//single parameter  non static method(here it is throwing compile time error)same combination repeated.
+        {
+            Console.WriteLine("one parameter print :" + a);
+            
+        }
+       */
+
+    }
+}
